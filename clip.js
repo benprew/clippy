7065fc51fe2d7150e2257d171bcf329a46538bce
Ben Prew <ben@throwingbones.com>
2018-09-20 22:15:46 -0700

    initial commit

---
+++ clip.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const axios = require('axios');
+const Readability = require("Readability");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const TurndownService = require('turndown')
+const turndownService = new TurndownService()
+
+const url = process.argv[2];
+
+axios(url).then((resp) => {
+  const html = resp.data;
+  const dom = new JSDOM(html);
+  var article = new Readability(dom.window.document).parse();
+  console.log(`---\ntitle: ${article.title}\n---\n`);
+  console.log(turndownService.turndown(article.content));
+});
