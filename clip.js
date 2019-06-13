550724840755a6c03b40454d7a3f7946e9445e89
Ben Prew <ben@throwingbones.com>
2019-06-13 15:16:55 -0700

    Upgrade packages. Remove axios, use JSDOM url support instead.

--- clip.js
+++ clip.js
@@ -2,19 +2,17 @@
 
 'use strict';
 
-const axios = require('axios');
 const Readability = require("Readability");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const TurndownService = require('turndown')
-const turndownService = new TurndownService()
+const TurndownService = require('turndown');
+const turndownService = new TurndownService();
 
 const url = process.argv[2];
 
-axios(url).then((resp) => {
-  const html = resp.data;
-  const dom = new JSDOM(html);
+JSDOM.fromURL(url).then(dom => {
   var article = new Readability(dom.window.document).parse();
+
   console.log(`---\ntitle: ${article.title}\nurl: ${url}\n---\n`);
   console.log(turndownService.turndown(article.content));
 });
