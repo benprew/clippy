fb688f0c6b6da7c0cc849e280d165291b3eaa58f
Ben Prew <bprew@comscore.com>
2020-02-24 16:45:09 -0800

    WIP: inline images in base64 encoded strings

--- clip.js
+++ clip.js
@@ -34,9 +34,60 @@ turndownService.addRule(
   }
 );
 
-JSDOM.fromURL(url).then(dom => {
+function get_url(url)
+{
+  return new Promise((resolve, reject) => {
+  var http;
+  if (url.startsWith("https")) {
+    http = require('https');
+  } else {
+    http = require('http');
+  }
+
+  var output = "";
+  var page = "";
+
+  // const req = http.request(new URL(url), , (res) => {
+  const req = http.get(new URL(url), (res) => {
+    // console.log(`STATUS: ${res.statusCode}`);
+    // console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.on('data', (chunk) => {
+      page += chunk;
+    });
+    res.on('end', () => {
+      console.log('done');
+      const imgStr = Buffer.from(page).toString('base64');
+      const contentType = res.headers['content-type'];
+
+      resolve(`data:${contentType};base64${imgStr}`);
+    });
+    res.on('abort', () => {
+      reject(console.log("aborted"));
+
+    });
+  });
+
+  req.on('error', (e) => {
+    reject(console.error(`problem with request: ${e.message}`));
+  });
+
+  });
+}
+
+async function main(url) {
+  const dom = await JSDOM.fromURL(url);
+  var imgs = dom.window.document.querySelectorAll("img");
+  for (var i=0; i < imgs.length; i++) {
+    // replace img src with base64 encoded data block
+    let dom_node = imgs[i];
+    const imgSrc = dom_node.getAttribute("src");
+    dom_node.setAttribute("src", await get_url("http://www.lebaneserecipes.com/" + imgSrc));
+    console.log(imgSrc);
+  }
   var article = new Readability(dom.window.document).parse();
 
   console.log(`---\ntitle: ${article.title}\nurl: ${url}\n---\n`);
   console.log(turndownService.turndown(article.content));
-});
+}
+
+main(url);
